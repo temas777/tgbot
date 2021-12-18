0be7a27468ebedf315d95d4228c5c61ff0108628
temas777 <91739590+temas777@users.noreply.github.com>
2021-12-18 19:15:39 +0200

    Delete handlers.py

--- handlers.py
+++
@@ -1,7 +0,0 @@
-import telebot
-token='5005657511:AAHKWoVnEYC8N7x5gbe_eJiCHaO_8Gdof8Y'
-bot=telebot.Telebot(token)
-@bot.message_handler(commands=['t'])
-def start_message(message):
-  bot.send_message(message.chat.id,"Привет ✌️ ")
-bot.infinity_poling()
